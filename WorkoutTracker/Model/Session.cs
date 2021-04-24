ecce8882da6a5cc71e943b2dd680ab48a299a4ff
Duh Choo <59976165+duhchoo@users.noreply.github.com>
2021-04-24 18:16:54 -0500

    added musclegroup, session, and muscletype enum

---
+++ WorkoutTracker/Model/Session.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Model
+{
+    public class Session
+    {
+        public int SessionID { get; set; }
+
+        public int MetricID { get; set; }
+
+        public int EnvironmentID { get; set; }
+
+        //public int StartTime { get; set; }
+
+        //public int EndTime { get; set; }
+
+        public int Rating { get; set; }
+
+        //public int Date { get; set; }
+
+    }
+}
