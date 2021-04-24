005061813545d45ca4bf476610789403fb26380f
Jacob Beck <jmbeck13@ksu.edu>
2021-04-24 18:24:40 -0500

    Added Workout and Weather model classes

---
+++ WorkoutTracker/Model/Workout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.DataAccess
+{
+    public class Workout
+    {
+        public int WorkoutID { get; }
+
+        public int SessionID { get; }
+
+        public int GroupNameID { get; }
+
+        public int Duration { get; }
+
+        public int AvgHeartRate { get; }
+    }
+}
