aef681c1aeb14c243abf5d353b3e09d05c839f4d
zachterrell57 <zachterrell@gmail.com>
2021-04-25 12:01:35 -0500

    Submit button goes back to home

--- WorkoutTracker/Pages/AddWorkout.cshtml.cs
+++ WorkoutTracker/Pages/AddWorkout.cshtml.cs
@@ -11,35 +11,52 @@ namespace WorkoutTracker.Pages
 {
     public class AddWorkoutModel : PageModel
     {
-
+        [BindProperty] //(SupportsGet = true) could need this
         public string Location { get; set; }
 
+        [BindProperty]
         public SqlDateTime Date { get; set; }
 
+        [BindProperty]
         public int Weight { get; set; }
 
+        [BindProperty]
         public int SleepDuration { get; set; }
 
+        [BindProperty]
         public int Calories { get; set; }
 
+        [BindProperty]
         public SqlDateTime StartTime { get; set; }
 
+        [BindProperty]
         public SqlDateTime EndTime { get; set; }
 
+        [BindProperty]
         public int Rating { get; set; }
 
+        [BindProperty]
         public int Duration { get; set; }
 
+        [BindProperty]
         public int AvgHeartRate { get; set; }
 
+        [BindProperty]
         public bool IsIndoor { get; set; }
 
-        public WeatherType Type { get; set;}
+        [BindProperty]
+        public WeatherType WeatherType { get; set;}
 
+        [BindProperty] 
         public int Name { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+
         }
     }
 }
