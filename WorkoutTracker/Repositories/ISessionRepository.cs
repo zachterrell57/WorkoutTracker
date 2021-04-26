4fdb49838dbacf30dea0ccbde4c579219d1cc6d6
Jacob Beck <jmbeck13@ksu.edu>
2021-04-25 22:54:54 -0500

    Seesion fix

--- WorkoutTracker/Repositories/ISessionRepository.cs
+++ WorkoutTracker/Repositories/ISessionRepository.cs
@@ -45,6 +45,6 @@ namespace WorkoutTracker
         /// <returns>
         /// The resulting instance of <see cref="Person"/>.
         /// </returns>
-        DailyMetrics CreateSession(string StartTime, string EndTime, double Rating);
+        Session CreateSession(string StartTime, string EndTime, double Rating);
     }
 }
