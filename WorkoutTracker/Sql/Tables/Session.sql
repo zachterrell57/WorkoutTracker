fbb8d4045de662a38094d2e5e0e649bf705ee4c4
Duh Choo <59976165+duhchoo@users.noreply.github.com>
2021-04-25 21:34:36 -0500

    session changes

--- WorkoutTracker/Sql/Tables/Session.sql
+++ WorkoutTracker/Sql/Tables/Session.sql
@@ -5,10 +5,8 @@ BEGIN
         SessionID INT NOT NULL IDENTITY(1, 1) PRIMARY KEY ,   
         MetricID INT NOT NULL FOREIGN KEY REFERENCES Project.DailyMetrics(MetricID),
         EnviornmentID INT NOT NULL FOREIGN KEY REFERENCES Project.Environment(EnvironmentID),
-        LastName NVARCHAR(32) NOT NULL,
-        StartTime NVARCHAR(10) NOT NULL, --Just putting in a time?--
-        EndTime NVARCHAR(10) NOT NULL, 
-        Rating INT NOT NULL, --Out of 10, 10 being best?--    
-        Date NVARCHAR(32) NOT NULL --Just have user enter in the day?--
+        StartTime NVARCHAR(32) NOT NULL, --Just putting in a time?--
+        EndTime NVARCHAR(32) NOT NULL, 
+        Rating FLOAT NOT NULL, --Out of 10, 10 being best?--    
     );
 END;
