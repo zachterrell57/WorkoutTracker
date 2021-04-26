c9cde7b747667757721167259dac95f6f19e3a21
Jacob Beck <jmbeck13@ksu.edu>
2021-04-25 21:15:27 -0500

    Created irepos and sqlrepos for weather and workout

---
+++ WorkoutTracker/Repositories/IWorkoutRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using WorkoutTracker.Model;
+
+namespace WorkoutTracker
+{
+    public interface IWorkoutRepository
+    {
+        ///// <summary>
+        ///// Retrieves all persons in the database.
+        ///// </summary>
+        ///// <returns>
+        ///// <see cref="IReadOnlyList{Person}"/> containing all persons.
+        ///// </returns>
+        //IReadOnlyList<DailyMetrics> RetrieveDailyMetrics();
+
+        /// <summary>
+        /// Fetches the person with the given <paramref name="MetricId"/> if it exists.
+        /// </summary>
+        /// <param name="personId">Identifier of the person to fetch.</param>
+        /// <returns>
+        /// An instance of <see cref="Person"/> containing the information of the requested person.
+        /// </returns>
+        /// <exception cref="DataAccess.RecordNotFoundException">
+        /// Thrown if <paramref name="personId"/> does not exist.
+        /// </exception>
+        //DailyMetrics FetchPerson(int personId);
+
+        ///// <summary>
+        ///// Gets the person with the given <paramref name="personId"/> if it exists.
+        ///// </summary>
+        ///// <param name="email">Email of the person to get.</param>
+        ///// <returns>
+        ///// An instance of <see cref="Person"/> containing the information of the requested person
+        ///// if one exists with with the provided <paramref name="email"/>.
+        ///// If one is not found, <c>null</c> is returned.
+        ///// </returns>
+        //Person GetPerson(string email);
+
+        /// <summary>
+        /// Creates a new person in the repository.
+        /// </summary>
+        /// <param name="firstName">First name of the person to create.</param>
+        /// <param name="lastName">Last name of the person to create.</param>
+        /// <param name="email">Email of the person to create.</param>
+        /// <returns>
+        /// The resulting instance of <see cref="Person"/>.
+        /// </returns>
+        Workout CreateWorkout(double Duration, double AvgHeartRate);
+    }
+}
