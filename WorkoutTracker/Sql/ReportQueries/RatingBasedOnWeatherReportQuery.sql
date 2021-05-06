8e40077c83941a6933a47adda07211e76e30dbe1
Duh Choo <59976165+duhchoo@users.noreply.github.com>
2021-05-05 20:12:17 -0500

    fixed typos

--- WorkoutTracker/Sql/ReportQueries/RatingBasedOnWeatherReportQuery.sql
+++ WorkoutTracker/Sql/ReportQueries/RatingBasedOnWeatherReportQuery.sql
@@ -2,6 +2,6 @@
 
 SELECT S.Rating AS 'Rating', W.WeatherType AS 'Weather Type'
 FROM Project.Session S
-	INNER JOIN Project.Environment E ON S.EnviornmentID = E.EnvironmentID
+	INNER JOIN Project.Environment E ON S.EnvironmentID = E.EnvironmentID
 	INNER JOIN Project.Weather W ON E.WeatherID = W.WeatherID
 WHERE W.WeatherType = 'Sunny' AND W.WeatherType = 'Rainy'
