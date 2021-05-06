4fa6384bc0067d4f775f14b0e7283df3fc112302
Jacob Beck <jmbeck13@ksu.edu>
2021-05-05 20:43:35 -0500

    iallworkout i dailymetric and iweather repos

--- WorkoutTracker/Repositories/IDailyMetricsRepository.cs
+++ WorkoutTracker/Repositories/IDailyMetricsRepository.cs
@@ -5,37 +5,6 @@ namespace WorkoutTracker
 {
     public interface IDailyMetricsRepository
     {
-        ///// <summary>
-        ///// Retrieves all persons in the database.
-        ///// </summary>
-        ///// <returns>
-        ///// <see cref="IReadOnlyList{Person}"/> containing all persons.
-        ///// </returns>
-        //IReadOnlyList<DailyMetrics> RetrieveDailyMetrics();
-
-        /// <summary>
-        /// Fetches the person with the given <paramref name="MetricId"/> if it exists.
-        /// </summary>
-        /// <param name="personId">Identifier of the person to fetch.</param>
-        /// <returns>
-        /// An instance of <see cref="Person"/> containing the information of the requested person.
-        /// </returns>
-        /// <exception cref="DataAccess.RecordNotFoundException">
-        /// Thrown if <paramref name="personId"/> does not exist.
-        /// </exception>
-        //DailyMetrics FetchPerson(int personId);
-
-        ///// <summary>
-        ///// Gets the person with the given <paramref name="personId"/> if it exists.
-        ///// </summary>
-        ///// <param name="email">Email of the person to get.</param>
-        ///// <returns>
-        ///// An instance of <see cref="Person"/> containing the information of the requested person
-        ///// if one exists with with the provided <paramref name="email"/>.
-        ///// If one is not found, <c>null</c> is returned.
-        ///// </returns>
-        //Person GetPerson(string email);
-
         /// <summary>
         /// Creates a new person in the repository.
         /// </summary>
